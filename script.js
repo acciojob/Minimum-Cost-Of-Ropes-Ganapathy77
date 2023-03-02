82db8ac3cefa1fe596f6dd2a958fd5e66a4089c2
Ganapathy77 <106886401+Ganapathy77@users.noreply.github.com>
2023-03-03 00:11:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 function calculateMinCost() {
   //your code here
-  
-  
-  
+	let result = document.querySelector("#result");
+  let ropeLenghts = document.querySelector("#rope-lengths").value;
+	let ropeLengthArray = ropeLenghts.split(",");
+	ropeLengthArray.sort(function (a,b) { return a-b;})
+	let cost = 0;
+
+	while(ropeLengthArray.length>1){
+		let sum= (Number(ropeLengthArray[0]) + Number(ropeLengthArray[1]));
+		cost+=sum;
+		ropeLengthArray.push(sum);
+		ropeLengthArray.shift();
+		ropeLengthArray.shift();
+
+		ropeLengthArray.sort(function (a,b) { return a-b; })
+		
+	}
+	result.innerHTML = cost;
+	result.classList.add("div");
 }  
